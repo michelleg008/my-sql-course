d00a3e815096c42b942f98f4c67c18e8ab7c7026
michelleg008 <michelle.geddes@combined.nhs.uk>
2025-07-10 11:01:05 +0000

    first select completed

---
+++ foundation.sql
@@ -0,0 +1,16 @@
+SELECT 
+    ps.PatientId
+    , ps.Hospital
+    , ps.Ward
+    , ps.AdmittedDate 
+    , ps.DischargeDate
+    , DATEDIFF(DAY, ps.AdmittedDate, ps.DischargeDate) AS LengthOfStay
+    , DATEADD(WEEK, -2, ps.AdmittedDate) AS ReminderDate
+    , ps.Ethnicity
+    , ps.Tariff
+FROM PatientStay ps
+WHERE ps.Hospital IN ('Kingston' , 'PRUH')
+AND ps.Ward LIKE '%Surgery'
+ORDER BY 
+    ps.AdmittedDate DESC 
+    , ps.PatientId DESC
