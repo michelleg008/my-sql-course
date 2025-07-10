b1471a5861de02c3a58409ac27ab987d14973cec
michelleg008 <michelle.geddes@combined.nhs.uk>
2025-07-10 11:29:35 +0000

    lunch save

--- foundation.sql
+++ foundation.sql
@@ -6,11 +6,12 @@ SELECT
     , ps.DischargeDate
     , DATEDIFF(DAY, ps.AdmittedDate, ps.DischargeDate) AS LengthOfStay
     , DATEADD(WEEK, -2, ps.AdmittedDate) AS ReminderDate
-    , ps.Ethnicity
     , ps.Tariff
 FROM PatientStay ps
 WHERE ps.Hospital IN ('Kingston' , 'PRUH')
 AND ps.Ward LIKE '%Surgery'
+AND ps.AdmittedDate BETWEEN '2024-02-27' AND '2024-03-01'
+AND ps.Tariff > 5
 ORDER BY 
     ps.AdmittedDate DESC 
     , ps.PatientId DESC
